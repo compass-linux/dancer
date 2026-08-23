e55391685ed077cf0539cc278934d0b1f7f01700
confucius40 <git@mail.katon15.xyz>
2026-08-23 18:55:21 +0200

    dquery bin

---
+++ src/DQueryMain.hs
@@ -0,0 +1,67 @@
+module Main where
+
+import System.Environment (getArgs)
+import System.Exit (exitFailure, exitSuccess)
+import Dancer.Types
+import Dancer.Logging
+import qualified Dancer.Config as Config
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    ["search", query] -> handleSearch query
+    ["uses", pkg]      -> handleUses pkg
+    _                  -> printUsage >> exitFailure
+
+handleSearch :: String -> IO ()
+handleSearch query = do
+  repos <- Config.listLocalRepos
+  if null repos
+    then do
+      logWarn "No local repositories found -- run 'dancer rebuild' at least once first"
+      exitFailure
+    else do
+      matches <- Config.searchPackages query repos
+      case matches of
+        [] -> do
+          putStrLn $ "No packages found matching \"" ++ query ++ "\""
+          exitFailure
+        _ -> do
+          putStrLn $ "Found " ++ show (length matches) ++ " package(s):"
+          mapM_ (\m -> putStrLn ("  " ++ m)) matches
+          exitSuccess
+
+handleUses :: String -> IO ()
+handleUses pkgArg = do
+  repos <- Config.listLocalRepos
+  if null repos
+    then do
+      logWarn "No local repositories found -- run 'dancer rebuild' at least once first"
+      exitFailure
+    else do
+      result <- Config.resolvePackageByName pkgArg repos
+      case result of
+        Nothing -> do
+          putStrLn $ "Package not found: " ++ pkgArg
+          exitFailure
+        Just pkg -> do
+          putStrLn $ pkgName pkg ++ " (" ++ pkgVersion pkg ++ ")"
+          if null (pkgUseFlags pkg)
+            then putStrLn "  No USE flags declared"
+            else do
+              putStrLn "  USE flags:"
+              mapM_ (\f -> putStrLn ("    " ++ f)) (pkgUseFlags pkg)
+          exitSuccess
+
+printUsage :: IO ()
+printUsage = putStr $ unlines
+  [ "dquery: Compass Linux package lookup"
+  , ""
+  , "Usage: dquery <command> [args]"
+  , ""
+  , "Commands:"
+  , "  search <name>    Search local package repos for a name/substring"
+  , "  uses <cat/pkg>   Show USE flags declared by a package"
+  , ""
+  ]
--- src/Dancer/Config.hs
+++ src/Dancer/Config.hs
@@ -1,10 +1,10 @@
 module Dancer.Config where
 
 import System.FilePath ((</>), takeFileName, takeDirectory)
-import System.Directory (doesFileExist, removeFile, createDirectoryIfMissing, doesDirectoryExist, removeDirectoryRecursive)
+import System.Directory (doesFileExist, removeFile, createDirectoryIfMissing, doesDirectoryExist, removeDirectoryRecursive, listDirectory)
 import System.Process (readProcess, system)
 import System.Exit (exitFailure, ExitCode(..))
-import Data.List (isInfixOf, intercalate)
+import Data.List (isInfixOf, intercalate, nub, sort)
 import Data.Maybe (catMaybes, listToMaybe)
 import Dancer.Types
 import Dancer.Logging
@@ -167,7 +167,52 @@ validateConfig config
   | null (buildFlags config) = Left "buildFlags cannot be empty"
   | otherwise = Right ()
 
-printConfig :: SystemConfig -> IO ()
+listLocalRepos :: IO [FilePath]
+listLocalRepos = do
+  exists <- doesDirectoryExist reposDir
+  if not exists
+    then return []
+    else do
+      entries <- listDirectory reposDir
+      dirs <- mapM (\e -> do
+                      let p = reposDir </> e
+                      isDir <- doesDirectoryExist p
+                      return (if isDir then Just p else Nothing)) entries
+      return (catMaybes dirs)
+
+listAllPackageNames :: [FilePath] -> IO [String]
+listAllPackageNames repoPaths = do
+  results <- mapM listPackagesInRepo repoPaths
+  return (nub (concat results))
+
+listPackagesInRepo :: FilePath -> IO [String]
+listPackagesInRepo repoPath = do
+  exists <- doesDirectoryExist repoPath
+  if not exists
+    then return []
+    else do
+      cats <- listDirectory repoPath
+      results <- mapM (listPackagesInCategory repoPath) cats
+      return (concat results)
+
+listPackagesInCategory :: FilePath -> String -> IO [String]
+listPackagesInCategory repoPath cat = do
+  let catPath = repoPath </> cat
+  isDir <- doesDirectoryExist catPath
+  if not isDir
+    then return []
+    else do
+      pkgs <- listDirectory catPath
+      results <- mapM (\p -> do
+                          let pkgFile = catPath </> p </> "package.hs"
+                          fileExists <- doesFileExist pkgFile
+                          return (if fileExists then Just (cat ++ "/" ++ p) else Nothing)) pkgs
+      return (catMaybes results)
+
+searchPackages :: String -> [FilePath] -> IO [String]
+searchPackages query repoPaths = do
+  allNames <- listAllPackageNames repoPaths
+  return (sort [n | n <- allNames, query `isInfixOf` n])
 printConfig config = do
   putStrLn "System Configuration:"
   putStrLn $ "  Hostname: " ++ hostname config
@@ -177,3 +222,22 @@ printConfig config = do
   putStrLn $ "  Packages: " ++ show (length (packages config))
   putStrLn $ "  Use Flags: " ++ show (useFlags config)
   putStrLn $ "  Repositories: " ++ show (length (repoSources config))
+
+lastConfigPath :: FilePath
+lastConfigPath = "/var/lib/dancer/generations/last-config"
+
+readLastConfig :: IO (Maybe SystemConfig)
+readLastConfig = do
+  exists <- doesFileExist lastConfigPath
+  if not exists
+    then return Nothing
+    else do
+      content <- readFile lastConfigPath
+      case reads content of
+        [(cfg, _)] -> return (Just cfg)
+        _ -> return Nothing
+
+writeLastConfig :: SystemConfig -> IO ()
+writeLastConfig config = do
+  createDirectoryIfMissing True "/var/lib/dancer/generations"
+  writeFile lastConfigPath (show config)
